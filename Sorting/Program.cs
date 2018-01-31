40136dde7c35400301e11b52e2cb7612ec435060
Peter Chenery <peter.chenery@anthonynolan.org>
2018-01-31 13:43:42 +0000

    SelectionSort Unit Test

--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -8,30 +8,40 @@ using System.Threading.Tasks;
 
 namespace Sorting
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        int[] Random1 = new int[10000];
+
+        public static void Main(string[] args)
         {
             int[] Random20 = new int[] { 12,7,19,3,15,18,9,5,16,4,11,13,2,10,1,6,20,8,14,17 };
             int[] Random40 = new int[] { 31,19,39,2,21,16,34,12,5,15,27,33,18,9,3,23,24,7,38,26,4,32,40,11,35,28,13,22,10,1,29,25,6,20,30,8,36,14,17,37 };
-            int[] Random1000 = new int[10000];
-            arrayGenerate(Random1000, 2);
-            int[] Random2000 = new int[20000];
-            arrayGenerate(Random2000, 2);
-
+            int[] Random1 = new int[10000];
+            arrayGenerate(Random1, 2);
+            int[] Random2 = new int[20000];
+            arrayGenerate(Random2, 2);
             Stopwatch stopWatch = new Stopwatch();
 
             stopWatch.Start();
-            selectionSort(Random1000);
+            selectionSort(Random1);
             stopWatch.Stop();
-            double ts = stopWatch.Elapsed.TotalMilliseconds;
+            showTiming("Selection Sort", Random1.Count(), stopWatch.Elapsed.TotalMilliseconds);
 
-            DisplayResults(Random1000);
-            Console.WriteLine("Run Time (ms) " + ts.ToString());
+            stopWatch.Start();
+            selectionSort(Random2);
+            stopWatch.Stop();
+            showTiming("Selection Sort", Random2.Count(), stopWatch.Elapsed.TotalMilliseconds);
+
+            //DisplayResults(Random1);
+        }
+
+        public static void showTiming(string name, int size, double time)
+        {
+            Console.WriteLine("{0} Run Time (ms) size {1} = {2} ", name, size, time.ToString());
             Console.ReadLine();
         }
 
-        static void arrayGenerate(int[] data, int Seed)
+        public static void arrayGenerate(int[] data, int Seed)
         {
             Random r = new Random(Seed);
             for (int i = 0; i < data.Count(); i++)
@@ -40,7 +50,7 @@ namespace Sorting
             }
         }
 
-        static void DisplayResults(int[] a)
+        public static void DisplayResults(int[] a)
         {
             for (int i = 0; i < a.Count(); i++)
             {
@@ -48,7 +58,7 @@ namespace Sorting
             }
         }
 
-        static void selectionSort(int[] a)
+        public static void selectionSort(int[] a)
         {
             int min, temp;
             int size = a.Count();
---
+++ SortingTest/SortingTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingTest
+{
+    public class SortingTest
+    {
+        [Test]
+        public void SelectionSortTest()
+        {
+            int[] Random1 = new int[10000];
+            Sorting.Program.arrayGenerate(Random1, 2);
+            Sorting.Program.selectionSort(Random1);
+
+            Assert.That(Random1, Is.Ordered);
+        }
+    }
+}
